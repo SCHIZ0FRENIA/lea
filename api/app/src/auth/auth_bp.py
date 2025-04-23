e3cda93c4e38926f45e07f44ceff0e1a7442dc5e
SCHIZ0FRENIA <tupa.artyom@gmail.com>
2025-04-23 13:07:58 +0300

    Clear project structure

--- api/app/src/auth/auth_bp.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-auth_bp = Blueprint('auth', __name__, url_prefix="/v1/auth")
-
-
-@auth_bp.route('register', methods = ['POST'])
-def register():
-    
